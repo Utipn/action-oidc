c49630dbe576bc3dc30b6bdd1ed3f6afa3dd484e
utip <utipn@yahoo.com>
2023-11-04 15:34:39 -0600

    s3 module

--- environments/s3/backend.tf
+++ environments/s3/backend.tf
@@ -1,5 +1,5 @@
 module "s3" {
-  source = "git::https://github.com/Utipn/s3-module-repo.git//s3-module?ref=v"
+  source = "git::https://github.com/Utipn/s3-module-repo.git//s3-module"
 }
 
 #Backend configuration
